f3380d909d03e0610d72db21009e7f1b050a4c01
Junior Hirwa <iamhirwejr@gmail.com>
2024-09-17 17:43:41 +0200

    feat: material widget and scaffold widgets

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,26 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-        appBar: AppBar(
-      title: const Text('flutter app'),
-      centerTitle: true,
-      backgroundColor: Colors.blue[600],
-    ),
+      appBar: AppBar(
+        title: const Text('Hello App Bar'),
+        centerTitle: true,
+        backgroundColor: Colors.blue[600],
+        titleTextStyle: const TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+      ),
       body: const Center(
-        child: Text('flutter application')
-      )
+        child: Text("Hello flutter app"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.blue[500],
+        tooltip: 'Add',
+        elevation: 10,
+        child: const Icon(Icons.add, color: Colors.white), // Increase the elevation for a more prominent button
+      ),
     ),
   ));
 }
