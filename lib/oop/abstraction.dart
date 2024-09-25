7e9704aa93d00e236d0ad6465df871583001f3f3
Junior Hirwa <iamhirwejr@gmail.com>
2024-09-25 15:48:58 +0200

    refactor: Abstraction

--- lib/oop/abstraction.dart
+++ lib/oop/abstraction.dart
@@ -1,3 +1,6 @@
+// abstraction
+// we don't need to know the underneath functionlity of a class, what we want is the instance of it and use it to perform our desired work. we don't want to know how the class was constructed or whatever it holds.
+
 class User {
   User({required this.name, required this.birthday});
   final String name;
