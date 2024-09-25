facc714dd8b57262a8796739d85eba86e2275f06
Junior Hirwa <iamhirwejr@gmail.com>
2024-09-25 15:44:48 +0200

    feat: introduction to object oriented programming in dart

---
+++ lib/oop/introduction.dart
@@ -0,0 +1,4 @@
+// OOP - Object oriented programming
+// OOP in dart uses the same concepts as in other languages that supports OOP, the concepts are: 
+
+// Abstraaction: The underneath functionality of a method or class is hidden from the end users since we only care about the output and we don't care about how the functionality is implemented.
